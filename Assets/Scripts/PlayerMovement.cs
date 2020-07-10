b5f3d44864919600925c8f75b2d9cc99fbca3b6b
Lithimlin <julinksky@googlemail.com>
2020-07-10 23:50:57 +0200

    add player turning

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody2D playerBody;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,13 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateFacing();
+    }
+
+    void UpdateFacing()
+    {
+        Vector2 faceDir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(faceDir.y, faceDir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(-angle, Vector3.back);
     }
 }
