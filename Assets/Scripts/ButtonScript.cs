43c60e4704a634a2385e4ffb63f12a468df71220
Flo722 <fabikru2001@gmail.com>
2020-07-11 23:33:07 +0200

    Added Goal

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    public LayerMask targetMask;
+    public int radius;
+    SpriteRenderer myRender;
+    public Sprite pressedSprite;
+    public List<Transform> playerTargets = new List<Transform>();
+    public bool isButton;
+    public GameObject target;
+    public LevelManager manager;
+    private void Start()
+    {
+        myRender = GetComponent<SpriteRenderer>();
+        StartCoroutine("FindTargetsWithDelay", .2F);
+    }
+
+
+
+    private void CheckButton()
+    {
+        Collider2D[] targetInViewRadius = Physics2D.OverlapCircleAll(transform.position, radius, targetMask);
+        foreach (Collider2D collider in targetInViewRadius)
+        {
+            Transform target = collider.GetComponent<Collider2D>().transform;
+            playerTargets.Add(target);
+       }
+    }
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            CheckButton();
+            if (playerTargets.Count > 0)
+            {
+                if (isButton)
+                {
+                    myRender.sprite = pressedSprite;
+                    Destroy(target.gameObject);
+                }
+                else
+                {
+                    //WIN
+                    manager.cases.Remove(gameObject);
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public List<GameObject> cases = new List<GameObject>();
+
+    private void Start()
+    {
+        foreach(GameObject casse in cases)
+        {
+            casse.GetComponent<ButtonScript>().manager = this;
+        }
+    }
+
+    private void Update()
+    {
+        if(cases.Count == 0)
+        {
+            //WIN
+            print("win");
+        }
+    }
+}
