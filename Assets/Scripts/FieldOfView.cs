5ffb4a999481753b3b9d33e6ff3b03e567f1d9f5
Flo722 <fabikru2001@gmail.com>
2020-07-11 11:13:13 +0200

    Old FOV

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -10,21 +10,26 @@ public class FieldOfView : MonoBehaviour
     {
         Mesh mesh = new Mesh();
 
-        Vector3[] vertices = new Vector3[3];
-        Vector2[] uv = new Vector2[3];
-        int[] triangles = new int[3];
+        Vector3[] vertices = new Vector3[4];
+        Vector2[] uv = new Vector2[4];
+        int[] triangles = new int[6];
 
-        vertices[0] = Vector3.zero;
+        vertices[0] = new Vector3 (0, viewLength);
         vertices[1] = new Vector3(viewLength, 0);
-        vertices[2] = new Vector3(0 ,-viewLength);
+        vertices[2] = new Vector3(viewLength, viewLength);
+        vertices[3] = new Vector3(0 , viewLength);
 
         triangles[0] = 0;
         triangles[1] = 1;
         triangles[2] = 2;
+        triangles[3] = 2;
+        triangles[4] = 1;
+        triangles[5] = 3;
 
         uv[0] = vertices[0];
         uv[1] = vertices[1];
-        uv[2] = vertices[1];
+        uv[2] = vertices[2];
+        uv[3] = vertices[2];
 
         mesh.vertices = vertices;
         mesh.uv = uv;
