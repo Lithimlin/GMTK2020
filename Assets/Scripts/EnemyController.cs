01907b3de7e76a0a2de045002310491653013cb0
Flo722 <fabikru2001@gmail.com>
2020-07-11 09:55:14 +0200

    Added Enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public List<Vector2> walkPoints;
+    public int moveSpeed;
+
+    Rigidbody2D myBody;
+    int currentPoint;
+
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        walkPoints[0] = transform.position;
+        currentPoint = 1;
+    }
+
+    void Update()
+    {
+        Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 targetPos = walkPoints[currentPoint] - currentPos;
+        myBody.velocity = ((targetPos).normalized * moveSpeed);
+    }
+}
