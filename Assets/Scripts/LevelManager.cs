f6d273df7b515c0c94c62ae90f111fbe0e2dd41f
Lithimlin <julinksky@googlemail.com>
2020-07-12 01:10:04 +0200

    integrate scene controller into level manager

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
     public List<GameObject> cases = new List<GameObject>();
+    public string nextLevel;
 
     private void Start()
     {
@@ -16,10 +18,26 @@ public class LevelManager : MonoBehaviour
 
     private void Update()
     {
-        if(cases.Count == 0)
+        if (Input.GetKeyDown("r"))
+        {
+            SwitchScene(SceneManager.GetActiveScene().name);
+        }
+
+        if (cases.Count == 0)
         {
             //WIN
             print("win");
+            SwitchScene(nextLevel);
         }
+    }
+
+    public void Death()
+    {
+        Debug.Log("Death...");
+    }
+
+    private void SwitchScene(string name)
+    {
+        SceneManager.LoadScene(name);
     }
 }
