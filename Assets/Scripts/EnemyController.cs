a856bdc14a2d5201fcc03c08ef15784ff382741c
Lithimlin <julinksky@googlemail.com>
2020-07-11 11:19:28 +0200

    add enemy movement and waiting

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,16 +5,20 @@ public class EnemyController : MonoBehaviour
 {
     public List<Vector2> waypoints;
     public int moveSpeed;
+    public float waitTime;
 
-    int currentPoint;
+    private int currentPoint;
+    private bool wait;
+    private float nextTime;
 
     void Start()
     {
         waypoints.Insert(0, transform.position);
         currentPoint = 1;
+        wait = false;
     }
 
-    void FixedUpdate()
+    void Update()
     {
         Patrol();
         Debug.Log(transform.position);
@@ -22,12 +26,27 @@ public class EnemyController : MonoBehaviour
 
     private void Patrol()
     {
-        Vector2 target = waypoints[currentPoint];
-        transform.LookAt(target);
-        transform.position = Vector2.MoveTowards(transform.position, target, .2f);
-        if (transform.position.Equals(target))
+        if (wait)
         {
-            currentPoint = (currentPoint + 1) % waypoints.Count;
+            if (Time.time > nextTime)
+            {
+                wait = false;
+            }
+        }
+        else
+        {
+            Vector2 target = waypoints[currentPoint];
+            Vector2 pos = transform.position;
+            Vector2 faceDir = target - pos;
+            float angle = Mathf.Atan2(faceDir.y, faceDir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(-angle, Vector3.back);
+            transform.position = Vector2.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+            if (transform.position.Equals(target))
+            {
+                wait = true;
+                nextTime = Time.time + waitTime;
+                currentPoint = (currentPoint + 1) % waypoints.Count;
+            }
         }
     }
 }
