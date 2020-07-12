4630d9c66e1685337e2526c484ec5108be022991
Flo722 <fabikru2001@gmail.com>
2020-07-12 10:21:31 +0200

    Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public Scene level1;
+    public void LoadLevelOne()
+    {
+        SceneManager.LoadScene(level1.name);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
