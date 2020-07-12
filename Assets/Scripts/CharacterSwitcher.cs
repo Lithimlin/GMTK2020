f8304a4ded33d7aaab29ad0e4cee5d9c4f693171
Flo722 <fabikru2001@gmail.com>
2020-07-12 16:55:29 +0200

    LevelChanges

--- Assets/Scripts/CharacterSwitcher.cs
+++ Assets/Scripts/CharacterSwitcher.cs
@@ -6,6 +6,9 @@ public class CharacterSwitcher : MonoBehaviour
 {
     public List<GameObject> players;
     public float cooldown = 5f;
+    public Camera sceneCam;
+    public float zoomStrengh;
+    public float zoomSpeed;
 
     [Header("Fill Bar")]
     public Canvas cooldownBar;
@@ -17,15 +20,19 @@ public class CharacterSwitcher : MonoBehaviour
 
     private int activePlayer = 0;
     private float time;
+    float oldSize;
+    bool zoomPhase;
 
     void Start()
     {
+        oldSize = sceneCam.orthographicSize;
     }
 
     void Update()
     {
         if (Input.GetKeyDown("space") && Time.time > time)
         {
+            ZoomOut();
             activePlayer = (activePlayer + 1) % players.Count;
             time = Time.time + cooldown;
             firstSwitch = false;
@@ -35,6 +42,29 @@ public class CharacterSwitcher : MonoBehaviour
 
     private void LateUpdate()
     {
+        //Zoom
+        if (!zoomPhase)
+        {
+            if (oldSize < sceneCam.orthographicSize)
+            {
+                sceneCam.orthographicSize -= 1 * Time.deltaTime * zoomSpeed;
+            }
+            else
+            {
+                sceneCam.orthographicSize = oldSize;
+            }
+        }else
+        {
+            if (oldSize + zoomStrengh > sceneCam.orthographicSize)
+            {
+                sceneCam.orthographicSize += 1 * Time.deltaTime * zoomSpeed;
+            }
+            else
+            {
+                zoomPhase = false;
+            }
+        }
+        //Switch and Bar
         cooldownBar.transform.position = GetActivePlayer().transform.position + Vector3.up * .8f + Vector3.right * .05f + Vector3.forward * 2f;
         int timeLeft = Mathf.RoundToInt(time - Time.time);
         if (timeLeft > 0)
@@ -52,5 +82,11 @@ public class CharacterSwitcher : MonoBehaviour
     public GameObject GetActivePlayer()
     {
         return players[activePlayer];
+    }
+
+    public void ZoomOut()
+    {
+        zoomPhase = true;
+        oldSize = sceneCam.orthographicSize;
     }
 }
