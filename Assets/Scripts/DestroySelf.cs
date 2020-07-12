663fcf1fef555c44459b8c485d875488a4af2504
Flo722 <fabikru2001@gmail.com>
2020-07-12 19:51:30 +0200

    Sound Added

---
+++ Assets/Scripts/DestroySelf.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour
+{
+    float time = 3;
+    private void Start()
+    {
+        time -= 1 * Time.deltaTime;
+        if(time < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
